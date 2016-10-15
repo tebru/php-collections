3c3d941380e59647a73cb688804839ccc0eb53c1
Nate Brunette <n@tebru.net>
2016-10-14 19:00:20 -0500

    Moving sublist back to ArrayList
    AbstractList does not have a constructor
    
    Signed-off-by: Nate Brunette <n@tebru.net>

--- src/AbstractList.php
+++ src/AbstractList.php
@@ -47,24 +47,4 @@ abstract class AbstractList extends AbstractCollection implements ListInterface
         // return -1 if not found or (size - index found - 1) to return the index of the element after reverse
         return false === $index ? -1 : $this->count() - $index - 1;
     }
-
-    /**
-     * Returns a new ListInterface ranging from $fromIndex inclusive to
-     * $toIndex exclusive
-     *
-     * @param int $fromIndex
-     * @param int $toIndex
-     * @return ListInterface
-     * @throws \OutOfBoundsException If to or from index does not exist
-     */
-    public function subList(int $fromIndex, int $toIndex): ListInterface
-    {
-        if (!$this->has($fromIndex) || !$this->has($toIndex - 1)) {
-            throw new OutOfBoundsException('Unable to create sub list as $toIndex or $fromIndex do not exist in list');
-        }
-
-        $newList = array_slice($this->toArray(), $fromIndex, $toIndex - $fromIndex);
-
-        return new static($newList);
-    }
 }
